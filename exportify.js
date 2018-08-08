9dac41721ad1de13be769100b8ee3e8f8c4ceb9f
Griffin Boyd <grifboyd@gmail.com>
2018-08-08 13:41:46 -0700

    Update exportify.js
    New client ID

--- exportify.js
+++ exportify.js
@@ -4,7 +4,7 @@ window.Helpers = {
 
     // Use Exportify application client_id if none given
     if (client_id == '') {
-      client_id = "9950ac751e34487dbbe027c4fd7f8e99"
+      client_id = "a17efab93ed4463ca04f06326cc60224"
     }
 
     window.location = "https://accounts.spotify.com/authorize" +
